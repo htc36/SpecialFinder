a6cea277c79ace7717b1b034de581656b2c1eaca
Harry Collard <htc36@uclive.ac.nz>
2020-01-04 23:04:14 +1300

    progress on menu bar, now can filter product type and choose which week of sales to veiw

--- functions2.php
+++ functions2.php
@@ -11,41 +11,25 @@
         	die("Connection failed: " . $conn->connect_error);
         }
 
-	$datatable = "products2";
-        $results_per_page = 100; // number of results per page
-        if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };
-        if (isset($_GET["filter"])) { $filter  = $_GET["filter"]; } else { $filter="1"; };
-        if (isset($_GET["order"])) { $order  = $_GET["order"]; } else { $order="asc"; };
-        if (isset($_GET["search"])) { $search  = $_GET["search"]; } else { $search=""; };
-        $start_from = ($page-1) * $results_per_page;
-        if ($search == "") {
-                $searchQuery = "";
-        } else {
-                $searchQuery = " where name LIKE('%".$search."%')";
-        }
+        $tableNameQuery = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'specials' order by table_name DESC";  
+        $tableNames = $conn->query($tableNameQuery);
+        $tableNames2 = $conn->query($tableNameQuery);
+        $tableRow = $tableNames2->fetch_assoc();
+        $tableName = "`".$tableRow['table_name']."`";
 
-        switch ($filter) {
-                case "1":
-                        $sql = "SELECT * FROM ".$datatable;
-                        $comboBoxText = "";
-                        break;
-                case "price":
-                        $sql = "SELECT * FROM ".$datatable.$searchQuery." ORDER BY salePrice ".$order." LIMIT $start_from, ".$results_per_page;
-                        $comboBoxText = "Price";
-                        break;
-                case "moneyOff":
-                        $sql = "SELECT * FROM ".$datatable.$searchQuery." ORDER BY origPrice - salePrice ".$order." LIMIT $start_from, ".$results_per_page;
-                        $comboBoxText = "Money Off";
-                        break;
-                case "markdown":
-                        $sql = "SELECT * FROM ".$datatable.$searchQuery." ORDER BY 1 - (origPrice / salePrice) ".$order." LIMIT $start_from, ".$results_per_page;
-                        $comboBoxText = "Markdown";
-                        break;
-        }
+        if (isset($_GET["date"])) { $tableName  = "`".$_GET["date"]."`"; } ;
 
+        $sql = "SELECT * FROM ".$tableName;
         $rs_result = $conn->query($sql);
-        #select * FROM itemsTest where itemsTest.name LIKE('%butter chicken%')
-	global $conn, $rs_result;
+        $typeQuery = "select distinct type FROM ".$tableName;
+        $type = $conn->query($typeQuery);
+        
+        
+        
+
+
+
+	global $conn, $rs_result, $type, $tableNames, $tableName;
 
 
 
