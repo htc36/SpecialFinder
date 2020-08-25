afe6d78bb03261a74668a598695181df9a7525a2
Harry Collard <htc36@uclive.ac.nz>
2020-08-25 23:10:32 +1200

    fixed error that breaks scraping

--- pythonScraper/fetchAll2.py
+++ pythonScraper/fetchAll2.py
@@ -11,7 +11,7 @@ from extraFunctions import *
 
 
 def getData(url):
-    time.sleep(random.uniform(20,40))
+    time.sleep(random.uniform(2,4))
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
     headers = {'Accept': '*/*', 'Connection': 'keep-alive', 'method': 'GET', 'accept-encoding': 'gzip, deflate, br', 'cache-control': 'no-cache', 'content-type': 'application/json', 'pragma': 'no-cache', 'sec-fetch-mode': 'cors', 'sec-fetch-site': 'same-origin', 'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36', 'x-requested-with': 'OnlineShopping.WebApp'}
     print(url)
@@ -27,6 +27,8 @@ def processData(connection, url, page, typee, date):
     items = response.json()['products']['totalItems']
     products = response.json()['products']['items']
     for i in products:
+        if (i['type'] != 'Product'):
+            continue
         startIndex = len(i['brand'])+1
         name = i['name'][startIndex : ] 
         if typee == "liquor-beer-cider":
@@ -96,42 +98,6 @@ def main():
         url = 'https://shop.countdown.co.nz/api/v1/products/search?dasFilter=Department%3B%3B' + iii + '%3Bfalse&nextUI=true&size=120&page=1&target=browse'
         url = 'https://shop.countdown.co.nz/api/v1/products?dasFilter=Department%3B%3B' + iii + '%3Bfalse&nextUI=true&size=120&page=1&target=browse'
         processData(connection, url, page, iii, date)
+    print("finished")
 main()
-
-
-
-
-
-
-
-
-
-
-
-locations = ["bakery", "deli-chilled-foods", "meat", "seafood", "baby-care", "baking-cooking", "biscuits-crackers", "breakfast-foods", "canned-prepared-foods", "chocolate-sweets-snacks", "cleaning-homecare", "drinks-hot-cold", "frozen-foods", "health-wellness", "home-kitchenware", "meal-ingredients", "office-entertainment", "personal-care", "pet-care", "liquor-beer-cider", "liquor-wine", "toys-party-needs"]
-
-
-
-
-
-locations = ["christmas", "bakery", "deli-chilled-foods", "meat", "seafood", "baby-care", "baking-cooking", "biscuits-crackers", "breakfast-foods", "canned-prepared-foods", "chocolate-sweets-snacks", "cleaning-homecare", "drinks-hot-cold", "frozen-foods", "health-wellness", "home-kitchenware", "meal-ingredients", "office-entertainment", "personal-care", "pet-care"]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-headers = {'Accept': '*/*', 'Connection': 'keep-alive', 'authority': 'shop.countdown.co.nz', 'method': 'GET', 'scheme': 'https', 'accept': 'application/json, text/plain, */*', 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'en-US,en;q=0.9', 'cache-control': 'no-cache', 'content-type': 'application/json', 'cookie': 'cw-sgbabtoe=5ed1fb895e08457a959bfea283f004a1; ai_user=Xr+iO|2019-11-24T08:08:09.775Z; _gcl_au=1.1.711282692.1574582890; _vwo_uuid_v2=DD44BEF2AEFBBB8A33CA735790235D3DD|ae6b2d640f2afb0b5fc02da15165c1c0; _ga=GA1.3.1002884010.1574582891; _vwo_uuid=DD44BEF2AEFBBB8A33CA735790235D3DD; gig_bootstrap_3_PWTq_MK-V930M4hDLpcL_qqUx224X_zPBEZ8yJeX45RHI-uKWYQC5QadqeRIfQKB=login; _fbp=fb.2.1574582890969.1964198752; _vwo_ds=3%3Aa_1%2Ct_0%3A0%241574582853%3A9.47216273%3A%3A%3A2_1%3A1; kampyle_userid=8656-b3d1-da98-89ac-a1f9-eb4f-6722-527b; cd_user_id=16e9c7436d74fc-0d7de31c50ff53-1528110c-1fa400-16e9c7436d8737; _hjid=43cd69d9-47be-48ca-974a-94bf4131afb8; ASP.NET_SessionId=vvwb00jr3fm2o2npi14rxtzd; cw-laie=821de8359141436586a91359ac1aad8c; __RequestVerificationToken=eBCmAQ_xgOdVfx9pCoRLBhRW4kEzvvSmbnzK6IQQupmZjYU497jhGdckwARy1a873qvRsrzSf956AT5xgZ_ONB3Bx6kTn_tF1crVY33gwtE1; _gid=GA1.3.1672741318.1575936298; _vis_opt_s=2%7C; _vis_opt_test_cookie=1; kampyleUserSession=1575936299641; kampyleUserSessionsCount=3; AKA_A2=A; cw-arjshtsw=red515944a28e4c02b127e73074db5972ralfcbqyu; _vwo_sn=1357767%3A7%3Arec2.visualwebsiteoptimizer.com%3A7%3A1; ai_sessioncw-=DOSI1|1575940667279|1575940811405.265; kampyleSessionPageCounter=8', 'expires': 'Sat, 01 Jan 2000 00:00:00 GMT', 'pragma': 'no-cache', 'referer': 'https://shop.countdown.co.nz/shop/specials/baking-cooking?nextUI=true', 'request-id': '|1b7e7d3f9f5a446f89edf57e8bddd044.be8c385f3b00412d', 'sec-fetch-mode': 'cors', 'sec-fetch-site': 'same-origin', 'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36', 'x-requested-with': 'OnlineShopping.WebApp'}
 
